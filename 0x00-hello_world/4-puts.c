14bab4df08966aa7f88f536d7ec092a9ac153bfc
jeremiah akhere <itzjeremyakhere@gmail.com>
2023-07-13 14:32:39 -0700

    prinf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Return : 0(Success)
-*/
+  *main - Entry point
+  *Return: always 0 (success)
+  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
